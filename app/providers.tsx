c16499ba22268f9e144587eda4f231e1e1bd7e0c
christopher Dikesa <chrismsmr@gmail.com>
2026-09-02 13:47:05 +0100

    Wrap context provider with SessionProvider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { createBrowserClient } from "@supabase/ssr";
+import { SessionProvider } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
 
 const Ctx = createContext<ReturnType<typeof createBrowserClient> | null>(null);
+
 export const useSupabase = () => useContext(Ctx)!;
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -14,12 +16,22 @@ export function Providers({ children }: { children: React.ReactNode }) {
       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     )
   );
+
   const router = useRouter();
 
   useEffect(() => {
-    const { data } = supabase.auth.onAuthStateChange(() => router.refresh());
+    const { data } = supabase.auth.onAuthStateChange(() => {
+      router.refresh();
+    });
+
     return () => data.subscription.unsubscribe();
   }, [router, supabase]);
 
-  return <Ctx.Provider value={supabase}>{children}</Ctx.Provider>;
+  return (
+    <SessionProvider>
+      <Ctx.Provider value={supabase}>
+        {children}
+      </Ctx.Provider>
+    </SessionProvider>
+  );
 }
